8947231e8181781339d7d142404eb9a54b9a3056
Alex Marchant <alexjmarchant@gmail.com>
2012-10-29 16:43:20 -0700

    commented out pry, ruby is now the only requirement

--- run.rb
+++ run.rb
@@ -1,6 +1,5 @@
 Dir[File.join("./classes/*.rb")].each {|f| require f}
-require "pry"
-
+# require "pry"
 
 # Type the letters on the game board in the quotes below, order doesn't matter
 board = Board.new("cslearckblolyrdsyczlpmppw")
